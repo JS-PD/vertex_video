35b39bbce7136f476349a364f94c1980da77a680
JS-PD <94349961+JS-PD@users.noreply.github.com>
2024-06-27 16:51:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,15 @@ import streamlit as st
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-# Streamlit Secrets에서 경로 가져오기
-credentials_path = st.secrets["gcp_service_account"]["credentials_path"]
+# GCS에서 파일 다운로드
+storage_client = storage.Client()
+bucket = storage_client.bucket("psychic-expanse-425002-r3-bucket-01")
+blob = bucket.blob("secret.json")
+with open('temp_credentials.json', 'wb') as file_obj:
+    blob.download_to_file(file_obj)
 
-# 클라우드 스토리지에서 JSON 파일 읽어오기
-credentials = service_account.Credentials.from_service_account_file(credentials_path)
+# 임시 파일에서 credential 로드
+credentials = service_account.Credentials.from_service_account_file("temp_credentials.json")
 
 # Google API 클라이언트 생성 (예: Google Sheets API)
 service = build('sheets', 'v4', credentials=credentials)
@@ -14,3 +18,4 @@ service = build('sheets', 'v4', credentials=credentials)
 # Google Sheets API 사용 예시
 sheet = service.spreadsheets().get(spreadsheetId='your-spreadsheet-id').execute()
 st.write(sheet)
+
