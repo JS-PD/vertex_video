6dda909e96657c264f708df91317e059b76a4916
JS-PD <94349961+JS-PD@users.noreply.github.com>
2024-06-27 17:27:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,4 @@ from google.cloud import storage
 from googleapiclient.discovery import build
 
 # Streamlit Cloud 환경 변수에서 서비스 계정 키 파일 내용 가져오기
-credentials_info = st.secrets["gcp_service_account"]["GOOGLE_APPLICATION_CREDENTIALS"]
-
-# 서비스 계정 인증 정보 생성
-credentials = service_account.Credentials.from_service_account_info(credentials_info)
-
-# GCS 클라이언트 초기화 (인증 정보 포함)
-storage_client = storage.Client(credentials=credentials)
+print(st.secrets["gcp_service_account"]["GOOGLE_APPLICATION_CREDENTIALS"])
