915fe3260c624b3531f5c702af0e9f2117e9471d
JS-PD <94349961+JS-PD@users.noreply.github.com>
2024-06-27 17:29:00 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from google.oauth2 import service_account
-from google.cloud import storage
-from googleapiclient.discovery import build
 
-# Streamlit Cloud 환경 변수에서 서비스 계정 키 파일 내용 가져오기
-print(st.secrets["gcp_service_account"]["GOOGLE_APPLICATION_CREDENTIALS"])
+api_key = st.secrets["api"]["key"]
+db_password = st.secrets["database"]["password"]
