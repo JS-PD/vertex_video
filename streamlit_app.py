a1566c6608da9fedf27b32447196685d3a6ffbc4
JS-PD <94349961+JS-PD@users.noreply.github.com>
2024-06-28 09:31:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,19 @@
 import os
 import json
-
 import streamlit as st
 
-# Secrets에서 JSON 데이터 로드
-credentials_info = json.loads(st.secrets["google_credentials"])
-credentials_path = '/tmp/credentials.json'
+try:
+    # JSON 데이터 파싱
+    credentials_info = json.loads(st.secrets["google_credentials"])
+    credentials_path = '/tmp/credentials.json'
 
-# 임시 파일에 인증 정보를 저장
-with open(credentials_path, 'w') as f:
-    json.dump(credentials_info, f)
+    # 임시 파일에 인증 정보 저장
+    with open(credentials_path, 'w') as f:
+        json.dump(credentials_info, f)
 
-
-print(credentials_path)
+    # 환경 변수 설정
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+except json.JSONDecodeError as e:
+    st.error(f"JSON parsing error: {e}")
+except Exception as e:
+    st.error(f"An error occurred: {e}")
