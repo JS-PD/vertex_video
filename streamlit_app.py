7883460c6a839b5c7dd132531d94688de80f1f6c
JS-PD <94349961+JS-PD@users.noreply.github.com>
2024-06-28 09:26:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+import streamlit as st
+
 # Secrets에서 JSON 데이터 로드
 credentials_info = json.loads(st.secrets["google_credentials"])
 credentials_path = '/tmp/credentials.json'
