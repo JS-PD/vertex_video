1f898bea17e979a800fdc5a56e277722edbc28e4
JS-PD <mailtojs@naver.com>
2024-07-02 14:54:58 +0900

    ttt

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,9 +60,10 @@ bucket_name = os.environ['GOOGLE_CLOUD_BUCKET_ID']
 bucket = storage.Client().bucket(bucket_name)
 #url = "https://youtu.be/ZNjS9M0qJak?si=qMA9ONScwqBU8JSN"
 
-video_url = st.text_input("Youtube URL을 입력하세요", 'https://youtu.be/WENUvclwo18?si=CngwTn2onM7PzZcP')
 
-prompt = st.text_input("영상 관련 질문을 입력하세요", placeholder='영상에 대해 자세히 설명해주세요')
+video_url = st.text_input("Youtube URL을 입력하세요", key="video_input", value='https://youtu.be/WENUvclwo18?si=CngwTn2onM7PzZcP')
+
+prompt = st.text_input("영상 관련 질문을 입력하세요", key="text_input", value=st.session_state.input_text, placeholder='ex) 영상에 대해 자세히 설명해주세요')
 
 process = st.button("영상 분석")
 
--- test.py
+++ test.py
@@ -1,13 +1,37 @@
 import streamlit as st
 
-# 세션 상태 초기화
-if 'input_text' not in st.session_state:
-    st.session_state.input_text = ''
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
 
-prompt = st.text_input("영상 관련 질문을 입력하세요", key="input_text", placeholder='영상에 대해 자세히 설명해주세요')
+col1, col2 = st.columns(2)
+
+with col1:
+    st.checkbox("Disable text input widget", key="disabled")
+    st.radio(
+        "Set text input label visibility 👉",
+        key="visibility",
+        options=["visible", "hidden", "collapsed"],
+    )
+    st.text_input(
+        "Placeholder for the other text input widget",
+        "This is a placeholder",
+        key="placeholder",
+    )
+
+with col2:
+    text_input = st.text_input(
+        "Enter some text 👇",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder=st.session_state.placeholder,
+    )
+
+    if text_input:
+        st.write("You entered: ", text_input)
 
 process = st.button("영상 분석")
 
 if process:
-    
-    prompt = st.session_state.input_text + "??"
+    st.write("You entered: ", text_input)
