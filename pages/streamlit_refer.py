684540c8b09d2ccedb522bda6fcee2e4f20bf727
JS-PD <mailtojs@naver.com>
2024-07-02 18:02:02 +0900

    ttt

--- pages/streamlit_refer.py
+++ pages/streamlit_refer.py
@@ -55,13 +55,12 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+
 key_value = os.environ.get('OPENAI_API_KEY')
 
 if key_value is None:
     os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]["OPENAI_API_KEY"]
     logger.info("Use Streamlit Secret Key")
-else:
-    logger.info("Use .env Secret Key")
 
 
 def main():
