99822cf6d79a4de885b1b6ec4ad9a6df6194d56a
JS-PD <mailtojs@naver.com>
2024-07-03 10:05:04 +0900

    ttt

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,13 +62,10 @@ bucket = storage.Client().bucket(bucket_name)
 
 
 
-if 'video_input' not in st.session_state:
-    st.session_state.video_input = 'https://youtu.be/WENUvclwo18?si=CngwTn2onM7PzZcP'
-
 if 'text_input' not in st.session_state:
     st.session_state.text_input = ''
 
-video_url = st.text_input("Youtube URL을 입력하세요", key="video_input", value=st.session_state.video_input)
+video_url = st.text_input("Youtube URL을 입력하세요", key="video_input", value='https://youtu.be/WENUvclwo18?si=CngwTn2onM7PzZcP')
 
 prompt = st.text_input("영상 관련 질문을 입력하세요", key="text_input", value=st.session_state.text_input, placeholder='ex) 영상에 대해 자세히 설명해주세요')
 
